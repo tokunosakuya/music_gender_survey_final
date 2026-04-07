76b44c6576ed1ad556caca525a32179f6af94011
tokunosakuya <minyoung_bae@ewhain.net>
2026-04-07 03:24:31 +0200

    Add files via upload

---
+++ tracks.js
@@ -0,0 +1,85 @@
+// 1. 원본 데이터 리스트 (60개)
+const RAW_TRACKS = [
+  { "id": "track_001", "file": "audio_trimmed/Suno_blues_feminine 3_cut.wav", "original_name": "Suno_blues_feminine 3_cut.wav", "genre": "blues", "style": "feminine" },
+  { "id": "track_002", "file": "audio_trimmed/Suno_blues_feminine 6_cut.wav", "original_name": "Suno_blues_feminine 6_cut.wav", "genre": "blues", "style": "feminine" },
+  { "id": "track_003", "file": "audio_trimmed/Suno_blues_masculine 2_cut.wav", "original_name": "Suno_blues_masculine 2_cut.wav", "genre": "blues", "style": "masculine" },
+  { "id": "track_004", "file": "audio_trimmed/Suno_blues_masculine 9_cut.wav", "original_name": "Suno_blues_masculine 9_cut.wav", "genre": "blues", "style": "masculine" },
+  { "id": "track_005", "file": "audio_trimmed/Suno_blues_neutral 17_cut.wav", "original_name": "Suno_blues_neutral 17_cut.wav", "genre": "blues", "style": "neutral" },
+  { "id": "track_006", "file": "audio_trimmed/Suno_blues_neutral 21_cut.wav", "original_name": "Suno_blues_neutral 21_cut.wav", "genre": "blues", "style": "neutral" },
+  { "id": "track_007", "file": "audio_trimmed/Suno_classical_feminine 4_cut.wav", "original_name": "Suno_classical_feminine 4_cut.wav", "genre": "classical", "style": "feminine" },
+  { "id": "track_008", "file": "audio_trimmed/Suno_classical_feminine 8_cut.wav", "original_name": "Suno_classical_feminine 8_cut.wav", "genre": "classical", "style": "feminine" },
+  { "id": "track_009", "file": "audio_trimmed/Suno_classical_masculine 25_cut.wav", "original_name": "Suno_classical_masculine 25_cut.wav", "genre": "classical", "style": "masculine" },
+  { "id": "track_010", "file": "audio_trimmed/Suno_classical_masculine 28_cut.wav", "original_name": "Suno_classical_masculine 28_cut.wav", "genre": "classical", "style": "masculine" },
+  { "id": "track_011", "file": "audio_trimmed/Suno_classical_neutral 21_cut.wav", "original_name": "Suno_classical_neutral 21_cut.wav", "genre": "classical", "style": "neutral" },
+  { "id": "track_012", "file": "audio_trimmed/Suno_classical_neutral 2_cut.wav", "original_name": "Suno_classical_neutral 2_cut.wav", "genre": "classical", "style": "neutral" },
+  { "id": "track_013", "file": "audio_trimmed/Suno_country_feminine 4_cut.wav", "original_name": "Suno_country_feminine 4_cut.wav", "genre": "country", "style": "feminine" },
+  { "id": "track_014", "file": "audio_trimmed/Suno_country_feminine 5_cut.wav", "original_name": "Suno_country_feminine 5_cut.wav", "genre": "country", "style": "feminine" },
+  { "id": "track_015", "file": "audio_trimmed/Suno_country_masculine 10_cut.wav", "original_name": "Suno_country_masculine 10_cut.wav", "genre": "country", "style": "masculine" },
+  { "id": "track_016", "file": "audio_trimmed/Suno_country_masculine 22_cut.wav", "original_name": "Suno_country_masculine 22_cut.wav", "genre": "country", "style": "masculine" },
+  { "id": "track_017", "file": "audio_trimmed/Suno_country_neutral 13_cut.wav", "original_name": "Suno_country_neutral 13_cut.wav", "genre": "country", "style": "neutral" },
+  { "id": "track_018", "file": "audio_trimmed/Suno_country_neutral 25_cut.wav", "original_name": "Suno_country_neutral 25_cut.wav", "genre": "country", "style": "neutral" },
+  { "id": "track_019", "file": "audio_trimmed/Suno_disco_feminine 10_cut.wav", "original_name": "Suno_disco_feminine 10_cut.wav", "genre": "disco", "style": "feminine" },
+  { "id": "track_020", "file": "audio_trimmed/Suno_disco_feminine 20_cut.wav", "original_name": "Suno_disco_feminine 20_cut.wav", "genre": "disco", "style": "feminine" },
+  { "id": "track_021", "file": "audio_trimmed/Suno_disco_masculine 13_cut.wav", "original_name": "Suno_disco_masculine 13_cut.wav", "genre": "disco", "style": "masculine" },
+  { "id": "track_022", "file": "audio_trimmed/Suno_disco_masculine 9_cut.wav", "original_name": "Suno_disco_masculine 9_cut.wav", "genre": "disco", "style": "masculine" },
+  { "id": "track_023", "file": "audio_trimmed/Suno_disco_neutral 22_cut.wav", "original_name": "Suno_disco_neutral 22_cut.wav", "genre": "disco", "style": "neutral" },
+  { "id": "track_024", "file": "audio_trimmed/Suno_disco_neutral 30_cut.wav", "original_name": "Suno_disco_neutral 30_cut.wav", "genre": "disco", "style": "neutral" },
+  { "id": "track_025", "file": "audio_trimmed/Suno_hiphop_feminine 26_cut.wav", "original_name": "Suno_hiphop_feminine 26_cut.wav", "genre": "hiphop", "style": "feminine" },
+  { "id": "track_026", "file": "audio_trimmed/Suno_hiphop_feminine 27_cut.wav", "original_name": "Suno_hiphop_feminine 27_cut.wav", "genre": "hiphop", "style": "feminine" },
+  { "id": "track_027", "file": "audio_trimmed/Suno_hiphop_masculine 21_cut.wav", "original_name": "Suno_hiphop_masculine 21_cut.wav", "genre": "hiphop", "style": "masculine" },
+  { "id": "track_028", "file": "audio_trimmed/Suno_hiphop_masculine 2_cut.wav", "original_name": "Suno_hiphop_masculine 2_cut.wav", "genre": "hiphop", "style": "masculine" },
+  { "id": "track_029", "file": "audio_trimmed/Suno_hiphop_neutral 27_cut.wav", "original_name": "Suno_hiphop_neutral 27_cut.wav", "genre": "hiphop", "style": "neutral" },
+  { "id": "track_030", "file": "audio_trimmed/Suno_hiphop_neutral 28_cut.wav", "original_name": "Suno_hiphop_neutral 28_cut.wav", "genre": "hiphop", "style": "neutral" },
+  { "id": "track_031", "file": "audio_trimmed/Suno_jazz_feminine 16_cut.wav", "original_name": "Suno_jazz_feminine 16_cut.wav", "genre": "jazz", "style": "feminine" },
+  { "id": "track_032", "file": "audio_trimmed/Suno_jazz_feminine 22_cut.wav", "original_name": "Suno_jazz_feminine 22_cut.wav", "genre": "jazz", "style": "feminine" },
+  { "id": "track_033", "file": "audio_trimmed/Suno_jazz_masculine 30_cut.wav", "original_name": "Suno_jazz_masculine 30_cut.wav", "genre": "jazz", "style": "masculine" },
+  { "id": "track_034", "file": "audio_trimmed/Suno_jazz_masculine 9_cut.wav", "original_name": "Suno_jazz_masculine 9_cut.wav", "genre": "jazz", "style": "masculine" },
+  { "id": "track_035", "file": "audio_trimmed/Suno_jazz_neutral 13_cut.wav", "original_name": "Suno_jazz_neutral 13_cut.wav", "genre": "jazz", "style": "neutral" },
+  { "id": "track_036", "file": "audio_trimmed/Suno_jazz_neutral 4_cut.wav", "original_name": "Suno_jazz_neutral 4_cut.wav", "genre": "jazz", "style": "neutral" },
+  { "id": "track_037", "file": "audio_trimmed/Suno_metal_feminine 17_cut.wav", "original_name": "Suno_metal_feminine 17_cut.wav", "genre": "metal", "style": "feminine" },
+  { "id": "track_038", "file": "audio_trimmed/Suno_metal_feminine 2_cut.wav", "original_name": "Suno_metal_feminine 2_cut.wav", "genre": "metal", "style": "feminine" },
+  { "id": "track_039", "file": "audio_trimmed/Suno_metal_masculine 5_cut.wav", "original_name": "Suno_metal_masculine 5_cut.wav", "genre": "metal", "style": "masculine" },
+  { "id": "track_040", "file": "audio_trimmed/Suno_metal_masculine 6_cut.wav", "original_name": "Suno_metal_masculine 6_cut.wav", "genre": "metal", "style": "masculine" },
+  { "id": "track_041", "file": "audio_trimmed/Suno_metal_neutral 1_cut.wav", "original_name": "Suno_metal_neutral 1_cut.wav", "genre": "metal", "style": "neutral" },
+  { "id": "track_042", "file": "audio_trimmed/Suno_metal_neutral 28_cut.wav", "original_name": "Suno_metal_neutral 28_cut.wav", "genre": "metal", "style": "neutral" },
+  { "id": "track_043", "file": "audio_trimmed/Suno_pop_feminine 11_cut.wav", "original_name": "Suno_pop_feminine 11_cut.wav", "genre": "pop", "style": "feminine" },
+  { "id": "track_044", "file": "audio_trimmed/Suno_pop_feminine 5_cut.wav", "original_name": "Suno_pop_feminine 5_cut.wav", "genre": "pop", "style": "feminine" },
+  { "id": "track_045", "file": "audio_trimmed/Suno_pop_masculine 14_cut.wav", "original_name": "Suno_pop_masculine 14_cut.wav", "genre": "pop", "style": "masculine" },
+  { "id": "track_046", "file": "audio_trimmed/Suno_pop_masculine 24_cut.wav", "original_name": "Suno_pop_masculine 24_cut.wav", "genre": "pop", "style": "masculine" },
+  { "id": "track_047", "file": "audio_trimmed/Suno_pop_neutral 1_cut.wav", "original_name": "Suno_pop_neutral 1_cut.wav", "genre": "pop", "style": "neutral" },
+  { "id": "track_048", "file": "audio_trimmed/Suno_pop_neutral 9_cut.wav", "original_name": "Suno_pop_neutral 9_cut.wav", "genre": "pop", "style": "neutral" },
+  { "id": "track_049", "file": "audio_trimmed/Suno_reggae_feminine 1_cut.wav", "original_name": "Suno_reggae_feminine 1_cut.wav", "genre": "reggae", "style": "feminine" },
+  { "id": "track_050", "file": "audio_trimmed/Suno_reggae_feminine 21_cut.wav", "original_name": "Suno_reggae_feminine 21_cut.wav", "genre": "reggae", "style": "feminine" },
+  { "id": "track_051", "file": "audio_trimmed/Suno_reggae_masculine 22_cut.wav", "original_name": "Suno_reggae_masculine 22_cut.wav", "genre": "reggae", "style": "masculine" },
+  { "id": "track_052", "file": "audio_trimmed/Suno_reggae_masculine 28_cut.wav", "original_name": "Suno_reggae_masculine 28_cut.wav", "genre": "reggae", "style": "masculine" },
+  { "id": "track_053", "file": "audio_trimmed/Suno_reggae_neutral 10_cut.wav", "original_name": "Suno_reggae_neutral 10_cut.wav", "genre": "reggae", "style": "neutral" },
+  { "id": "track_054", "file": "audio_trimmed/Suno_reggae_neutral 21_cut.wav", "original_name": "Suno_reggae_neutral 21_cut.wav", "genre": "reggae", "style": "neutral" },
+  { "id": "track_055", "file": "audio_trimmed/Suno_rock_feminine 19_cut.wav", "original_name": "Suno_rock_feminine 19_cut.wav", "genre": "rock", "style": "feminine" },
+  { "id": "track_056", "file": "audio_trimmed/Suno_rock_feminine 28_cut.wav", "original_name": "Suno_rock_feminine 28_cut.wav", "genre": "rock", "style": "feminine" },
+  { "id": "track_057", "file": "audio_trimmed/Suno_rock_masculine 12_cut.wav", "original_name": "Suno_rock_masculine 12_cut.wav", "genre": "rock", "style": "masculine" },
+  { "id": "track_058", "file": "audio_trimmed/Suno_rock_masculine 8_cut.wav", "original_name": "Suno_rock_masculine 8_cut.wav", "genre": "rock", "style": "masculine" },
+  { "id": "track_059", "file": "audio_trimmed/Suno_rock_neutral 15_cut.wav", "original_name": "Suno_rock_neutral 15_cut.wav", "genre": "rock", "style": "neutral" },
+  { "id": "track_060", "file": "audio_trimmed/Suno_rock_neutral 9_cut.wav", "original_name": "Suno_rock_neutral 9_cut.wav", "genre": "rock", "style": "neutral" }
+];
+
+// 2. Supabase 설정 정보
+const SUPABASE_PROJECT_ID = 'zididtqghlsxszjkpiuk';
+const BUCKET_NAME = 'audio';
+
+// 3. 파일 경로를 Supabase URL로 자동 매핑
+const TRACKS = RAW_TRACKS.map(track => {
+  const encodedPath = track.file
+    .split('/')
+    .map(part => encodeURIComponent(part))
+    .join('/');
+
+  return {
+    ...track,
+    file: `https://${SUPABASE_PROJECT_ID}.supabase.co/storage/v1/object/public/${BUCKET_NAME}/${encodedPath}`
+  };
+});
+
+window.TRACKS = TRACKS;
+
+console.log("TRACKS loaded:", TRACKS.length);
+console.log("First URL:", TRACKS[0].file);
